fcd9eaf0f0cce6131816b0da944965fe80eea063
MatteoCharrierITS <matteo.charrier@edu-its.it>
2025-12-15 11:35:48 +0100

    Aggiornamento appunti

---
+++ Esercizi Python/es 15.12.2025.py
@@ -0,0 +1,7 @@
+lst = []
+
+for x in range(5):
+    y = input("Inserisci il numero: ")
+    lst.append(y)
+    
+print(lst)
---
+++ skhwnek.py
@@ -0,0 +1,6 @@
+lst = []
+
+for x in range(2):
+    lst.append(input("Inserisci un numero: "))
+    
+print (lst)
